4553e228577d61dc45649fac9582d8e672c665f3
JoejosephKoonthanathshibu <koon0026@algonquinlive.com>
2024-05-21 08:53:03 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+//TASK - 1;
+
+let a=3;
+let b=5;
+let c;
+
+let expression =
+`
+let a=3;
+let b=5;
+let c;
+
+-----------
+
+a+b=${a+b}
+a-b=${a-b}
+a*b=${a*b}
+a/b=${a/b}
+a%b=${a%b}
+a += b =${a+= b}
+a -=b =${a -= b}
+a *=b =${a *= b}
+a /=b =${a /= b}
+a %=b =${a %= b}
+a == b =${a == b}
+a !=b =${a != b}
+a > b =${a > b}
+a < b =${a < b}
+!a && !c =${!a && !c}
+!a || !c =${!a || !c}
+`
+console.log(expression);
+
+//TASK - 2
+
+let first_name="Joe Joseph";
+let last_name="Koonthanath Shibu";
+let email="koon0026@algonquinlive.com";
+
+let output = "My Name is " + first_name + " " + last_name + "." + "You can contact me at " + email;
+
+console.log(output);
